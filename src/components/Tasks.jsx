08574b64b38819bfbcf912605161c2e0c947c88a
Paulo Lopez <geekcrasher@gmail.com>
2023-06-25 18:45:44 +0800

    change the design of the task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Trash } from "lucide-react";
+import { XCircle } from "lucide-react";
 
 const Tasks = ({ todos, deleteTodoMutation }) => {
   return (
@@ -7,32 +7,34 @@ const Tasks = ({ todos, deleteTodoMutation }) => {
       {todos?.data?.map((task) => {
         return (
           <section
-            className="h-52 bg-task dark:bg-task_dark rounded-lg shadow-md px-6 py-2 transition ease-in-out duration-100 hover:scale-95 cursor-pointer"
+            className="relative h-56 bg-task dark:bg-task_dark rounded-lg shadow-md px-6 py-2 transition ease-in-out duration-100 hover:scale-95 cursor-pointer"
             key={task.id}
           >
-            <div className=" flex items-center bg-semi mt-2">
-              <h1 className="font-noto-sans font-semibold text-text_light dark:text-primary xs:text-md sm:text-base lg:text-lg ">
-                {task.title}
-              </h1>
-            </div>
-            <div className="h-24 mt-3">
-              <p className="text-sm text-secondary font-semibold break-words overflow-hidden">
-                {task.description}
-              </p>
-            </div>
-            <div className="flex items-center justify-between mt-3 ">
+            <section className="relative h-full">
+              <div className=" flex items-center bg-semi mt-2 min-h-fit">
+                <h1 className="font-noto-sans font-semibold text-text_light dark:text-primary xs:text-md sm:text-base lg:text-lg ">
+                  {task.title}
+                </h1>
+              </div>
+              <div className="h-22 mt-2 overflow-hidden overflow-y-scroll scrollbar-hide">
+                <p className=" text-secondary dark:text-semi_dark tracking-wide text-sm break-words overflow-hidden">
+                  {task.description}
+                </p>
+              </div>
+              <div className="absolute bottom-4 w-full flex items-center justify-between mt-7 md:mt-5">
                 <p className="w-max text-xs font-bold px-2 py-1 rounded-full text-secondary bg-highlight dark:bg-priority">
                   {task.priority}
                 </p>
                 <button
                   onClick={() => deleteTodoMutation.mutate(task.id)}
-                  className="p-1.5 rounded-lg bg-highlight transition-colors duration-100 hover:bg-pink_primary"
+                  className="xs:p-1 md:p-1.2 rounded-md transition-colors duration-100 bg-pink_primary hover:bg-pink_secondary active:bg-pink_primary"
                 >
                   <span>
-                    <Trash size={14} color="#6D6D72" />
+                    <XCircle size={16} />
                   </span>
                 </button>
               </div>
+            </section>
           </section>
         );
       })}
