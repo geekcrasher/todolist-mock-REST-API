86667744aa5f8683fd0f2a048908270e2d06ec58
Paulo Lopez <geekcrasher@gmail.com>
2023-06-29 23:43:27 +0800

    added proxy for CORS issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/todos": "http://localhost:4000",
+    },
+  },
   plugins: [react()],
-  base: "/todolist-mock-REST-API/"
-})
+  base: "/todolist-mock-REST-API/",
+});
